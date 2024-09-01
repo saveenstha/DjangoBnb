09f833c6a41f880f345bdaf050efe9fa226aa262
saveenstha <biz.saveen@gmail.com>
2024-09-01 14:17:55 +0545

    code for Navbar

--- djangobnb/app/layout.tsx
+++ djangobnb/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/app/components/navbar/Navbar";
 
+import Navbar from "@/app/components/navbar/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,10 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          <Navbar />
-          <div className="pt-32">
-            {children}
-          </div>
+        <Navbar />
+        <div className="pt-32">
+          {children}
+        </div>
       </body>
     </html>
   );
