0e07c66976a95729cec75c110440163f40d30e53
saveenstha <biz.saveen@gmail.com>
2024-09-01 11:52:40 +0545

    Install and set up nextjs, create project

---
+++ djangobnb/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+        Django Bnb
+        <h2 className="text-airbnb"> Django and next project</h2>
+
+    </main>
+  );
+}
