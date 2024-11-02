e068e3d6282c31cf25bbc58d5eccb741d565e1ab
saveenstha <biz.saveen@gmail.com>
2024-11-02 23:50:09 +0545

    get properties in the frontend and list them

--- djangobnb/app/components/properties/PropertyList.tsx
+++ djangobnb/app/components/properties/PropertyList.tsx
@@ -1,12 +1,50 @@
+'use client';
+
+import {useEffect, useState} from "react";
 import PropertyListItem from "@/app/components/properties/PropertyListItem";
+
+export type PropertyType = {
+    id: string;
+    title: string;
+    price_per_night: number;
+    image_url: string;
+}
+
 const PropertyList = () => {
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+
+    const getProperties = async () => {
+        const url = 'http://localhost:8000/api/properties/'
+
+        await fetch(url, {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('json', json);
+
+                setProperties(json.data)
+            })
+            .catch((error) => {
+                console.log('error', error);
+            });
+    };
+    useEffect(() => {
+        getProperties();
+    }, []);
+
     return (
         <>
-        <PropertyListItem/>
-        <PropertyListItem/>
-        <PropertyListItem/>
+            {properties.map((property) =>{
+                return(
+                    <PropertyListItem
+                        key={property.id}
+                        property={property}
+                    />
+                )
+            })}
 
-    </>
+        </>
     )
 }
 
